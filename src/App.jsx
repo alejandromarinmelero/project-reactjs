d6d6506490b6682d6536c47e845b1d3b7e3ca63e
alejandromarinmelero <alex_clasiko@hotmail.es>
2022-06-13 17:46:03 -0300

    segundo desafio entregable

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import NavBar from './componentes/NavBar';
+
+function App() {
+
+  // Aquí fuera va toda la lógica JavaScript
+
+  return ( // Dentro del return va el código JSX para mostrar en el navegador
+  <div className='container'>
+      <NavBar />
+  </div>
+  );
+}
+
+export default App;
---
+++ src/componentes/NavBar.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import './navbar.css';
+import logo from './logo192.png'
+
+function NavBar () {
+    return (
+            <div className='navbar'>
+                <ul className='links'>
+                    <li>Tienda</li>
+                    <li>Galería</li>
+                    <li>Contacto</li>
+                </ul>
+                <div className='logo'>
+                    <img src={logo} className="logo"/>
+                    <h2>B-NylFactory</h2>
+                </div>
+                <ul className='login-links'>
+                    <li>Acceder</li>
+                    <li>Registrarse</li>
+                </ul>
+            </div>
+    )
+}
+
+export default NavBar;
